a5420fd5cfa5af00ed0323ca07e7c409ba8edec3
Ryan Hall <ryanhall@Mjolnir.lan>
2020-07-28 04:11:35 -0400

    index change

--- JS/particleintro.js
+++ JS/particleintro.js
@@ -17,7 +17,7 @@ function init() {
 	var particleMat = new THREE.PointsMaterial({
 		color:'rgb(255,255,255)',
 		size: 1,
-		map: new THREE.TextureLoader().load('/assets/textures%20/particle.jpg'),
+		map: new THREE.TextureLoader().load('../assets/textures%20/particle.jpg'),
 		transparent: true,
 		blending: THREE.AdditiveBlending,
 		depthWrite: false
